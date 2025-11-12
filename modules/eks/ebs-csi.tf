72a14b4f076bc1a74787f67f6775be7b1f130b49
Abdelaziz-Gbr <abdelazizkhalid@outlook.com>
2025-11-12 15:23:50 +0200

    changes

--- modules/eks/ebs-csi.tf
+++ modules/eks/ebs-csi.tf
@@ -32,11 +32,10 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
   role       = aws_iam_role.ebs_csi_driver.name
 }
 
-# Install EBS CSI Driver as EKS addon
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name             = aws_eks_cluster.main.name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.37.0-eksbuild.1"  # Check latest version for your EKS version
+  addon_version            = "v1.37.0-eksbuild.1" 
   service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
 
   depends_on = [
--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -12,13 +12,3 @@ output "oidc_provider_arn" {
   description = "ARN of the OIDC provider"
   value       = aws_iam_openid_connect_provider.eks.arn
 }
-
-output "jenkins_role_arn" {
-  description = "IAM role ARN for Jenkins service account"
-  value       = aws_iam_role.jenkins.arn
-}
-
-output "jenkins_role_name" {
-  description = "IAM role name for Jenkins"
-  value       = aws_iam_role.jenkins.name
-}
